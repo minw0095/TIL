0645a0766d5d0aa199948699ea3286dc61e56d95
minw0095 <kmu6898@naver.com>
2022-12-28 23:43:23 +0900

    1978,10430 풀이

---
+++ python/수학/1929.py
@@ -0,0 +1,14 @@
+# M, N = map(int, input().split())
+
+def prime(n):
+    check = [True] * n
+
+    m = int(n ** 0.5)
+    for a in range(2,m+1):
+        if check[a]:
+            for b in range(a+a, n, a):
+                check[b] = False
+    
+    return [i for i in range(2,n) if check[i]==True]
+
+print(prime(20))
---
+++ python/수학/6588.py
@@ -0,0 +1,27 @@
+# n = int(input())
+
+# if n >5:
+#     nums = []
+#     real = set()
+#     for a in range(1,n+1):
+#         rest = []
+#         for t in range(1,a+1):
+#             if a % t == 0 and a%2 == 1:
+#                 rest.append(t)
+#         if len(rest) == 2:
+#             nums.append(a)
+    
+#     if len(nums) > 0:
+#         for i in nums:
+#             if n-i in nums:
+#                 real.add(i)
+    
+#     if len(real) > 0:
+#         print(f'{n} = {n-max(real)} + {max(real)}')
+#     else:
+#         print("Goldbach's conjecture is wrong.")
+    
+
+    
+
+
