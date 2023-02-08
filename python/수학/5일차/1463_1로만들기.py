15bc09dd15cb5be8ff8af5c350d1c5dd41273e9f
minw0095 <kmu6898@naver.com>
2023-02-08 22:57:03 +0900

    알고 풀이

--- python/수학/5일차/1463_1로만들기.py
+++ python/수학/5일차/1463_1로만들기.py
@@ -3,12 +3,12 @@ dp =[0]*(n+1)
 
 
 for a in range(2,n+1):
-    cnt = dp[a-1]+1
-    if a % 2 == 0:
-        if cnt >= dp[a//2]+1:
-            cnt = dp[a//2]+1
+    cnt = dp[a-1]+1 # cnt는 현재 dp의 값을 저장, 2와 3으로 나누어 지지 않을 경우 이전 dp+1 한 것이 최소값
+    if a % 2 == 0: # 만약 2로 나누어 지고
+        if cnt > dp[a//2]+1: # 2로 나누어진 값의 dp 값이 위의 cnt 값보다 작을시(최소가 아니므로)
+            cnt = dp[a//2]+1  # 현재 dp의 값은 2로 나누어진 값의 dp +1이 된다
     if a % 3 == 0:
-        if cnt >= dp[a//3]+1:
+        if cnt > dp[a//3]+1:
             cnt = dp[a//3]+1
     
     dp[a] = cnt
--- python/수학/5일차/2504_괄호의값.py
+++ python/수학/5일차/2504_괄호의값.py
@@ -1,7 +1,54 @@
 N = input()
 
-satck = []
+stack = []
+num = 1
+res = 0
+
 for a in range(len(N)):
+
     if N[a] == '(':
-        if N[a+1] != ')':
-            
+        stack.append(N[a])
+        num *= 2
+
+    elif N[a] == '[':
+        stack.append(N[a])
+        num *= 3
+    
+    elif N[a] == ')':
+        if not stack :
+            res =0
+            break
+
+        
+        if N[a-1] == '(':
+            res += num
+            num //= 2
+            stack.pop()
+
+        if N[a-1] != '(' and stack:
+            if stack[-1] == '(':
+                
+                num //= 2
+                stack.pop()
+
+    elif N[a] == ']':
+        if not stack  :
+            res = 0
+            break
+
+        if N[a-1] == '[':
+            res += num
+            num //= 3
+            stack.pop()
+        
+        if N[a-1] != '[' and stack:
+            if stack[-1] == '[':
+                
+                num //= 3
+                stack.pop()
+
+if stack:
+    print(0)
+
+if not stack:
+    print(res)
