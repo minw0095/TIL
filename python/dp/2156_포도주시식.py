e9038811d130abf21274d4a7d605ff89105a9ee4
minw0095 <kmu6898@naver.com>
2023-02-10 21:04:49 +0900

    2156 풀이

--- python/dp/2156_포도주시식.py
+++ python/dp/2156_포도주시식.py
@@ -1,6 +1,19 @@
-n = int(input())
-j = [int(input()) for _ in range(n)]
+import sys
+input = sys.stdin.readline
 
-res = 0
-for a in range(n):
-    res = j[a] + 
+n = int(input())
+j = [0] * 10001
+for t in range(1,n+1):
+    j[t] = int(input())
+
+dp = [0] * (10000+1)
+dp[1] = j[1]
+dp[2] = j[1] + j[2]
+dp[3] = max(dp[2],j[1]+j[3], j[2]+j[3])
+
+for a in range(4,n+1):
+    dp[a] = max(dp[a-1], dp[a-3]+j[a]+j[a-1], dp[a-2]+j[a])
+
+
+print(dp[n])
+ 
