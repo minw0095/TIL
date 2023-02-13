1a00f95d8fd1add2d7fda5674d620a8a1fa0b4de
minw0095 <kmu6898@naver.com>
2023-02-13 18:51:33 +0900

    dp 문제

--- python/dp/10844_쉬운계단수.py
+++ python/dp/10844_쉬운계단수.py
@@ -1,5 +1,3 @@
-N = int(input())
-
 # res = 0
 # for a in range(1001,10000):
 #     cnt = 0
@@ -9,13 +7,24 @@ N = int(input())
 #     if cnt == 3:
 #         res +=1
 # print(res)
+N = int(input())
 
-res = 0
-for a in range(1000000,10000000):
-    cnt = 0
-    for b in range(6):
-        if abs(int(str(a)[b]) -int(str(a)[b+1])) ==1:
-            cnt += 1
-    if cnt == 6:
-        res +=1
-print(res)
+dp = [[0 for _ in range(10)] for _ in range(101)]
+
+for a in range(1,10):
+    dp[1][a] = 1
+
+
+for a in range(2,N+1):
+    for b in range(10):
+        if b == 0:
+            dp[a][b] = dp[a-1][1]
+        
+        elif b == 9:
+            dp[a][b] = dp[a-1][8]
+
+        else:
+            dp[a][b] = dp[a-1][b-1] + dp[a-1][b+1]
+
+
+print(sum(dp[N])% 1000000000)
