151ff2fb43814db06ef85d881da33fe152fcfca0
minw0095 <kmu6898@naver.com>
2023-02-28 23:35:22 +0900

    오르막수

---
+++ python/수학/7일차/14502_연구소.py
@@ -0,0 +1,46 @@
+import sys
+sys.stdin = open("input.txt", "r")
+from collections import deque
+import copy
+
+N,M = map(int, input().split())
+
+graph = [list(map(int, input().split())) for _ in range(N)]
+graph2 = copy.deepcopy(graph)
+
+def bfs(x,y):
+    q = deque()
+    q.append((x,y))
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
+
+
+    while q:
+        x,y = q.popleft()
+        
+        for a in range(4):
+            nx = x + dx[a]
+            ny = y + dy[a]
+            
+            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 0:
+                graph[nx][ny] = 2
+                q.append([nx,ny])
+
+def wall(t):
+    if t == 3:
+        bfs()
+
+
+for a in range(N):
+    for b in range(M):
+        if graph[a][b] == 2:
+            bfs(a,b)
+
+for a in graph:
+    for b in a:
+        if b == 0:
+            b = 1
+
+print(graph)
+        
+
