609f6c9feef11dc8249565863a0aa237a10f9520
minw0095 <kmu6898@naver.com>
2023-03-07 15:02:10 +0900

    스터디

--- python/수학/7일차/14501_퇴사.py
+++ python/수학/7일차/14501_퇴사.py
@@ -1,6 +1,21 @@
 N = int(input())
 
-dp = []
+T = []
+P = []
+
+dp = [0 for _ in range(N+1)]
 
 for a in range(1,N+1):
-    t,p = 
+    t,p = map(int, input().split())
+    T.append(t)
+    P.append(p)
+
+for a in range(N-1, -1,- 1):
+    if T[a] + a > N:
+        dp[a] = dp[a+1]
+    
+    else:
+        dp[a] = max(dp[a+1], dp[T[a] + a] + P[a])
+
+print(dp[0])
+
