89cba45c79a9b1afcc3c43aa525e871f4fb09b1b
minw0095 <kmu6898@naver.com>
2023-03-24 15:32:01 +0900

    1766_문제집

--- python/힙/1766_문제집.py
+++ python/힙/1766_문제집.py
@@ -7,21 +7,37 @@ n,m = map(int, input().split())
 
 indegree = [0] * (n+1)
 
-graph = [[] for _ in range(m+1)]
+graph = [[] for _ in range(n+1)]
 
 for _ in range(m):
     x,y = map(int, input().split())
     graph[x].append(y)
-    indegree[x] += 1
+    indegree[y] += 1
+
 
 
 def d_sort():
     res = []
-    q = deque()
+    q = []
 
     for a in range(1,n+1):
         if indegree[a] == 0:
             q.append(a)
+
+    while q:
+        t = heapq.heappop(q)
+        res.append(t)
+
+        for a in graph[t]:
+            indegree[a] -= 1
+
+            if indegree[a] == 0:
+                heapq.heappush(q,a)
+    
+    for a in res:
+        print(a, end=' ')
+
+d_sort()
 
 
 
--- python/힙/test.py
+++ python/힙/test.py
@@ -1,16 +1,44 @@
 import heapq
+from collections import deque
+import sys
+input = sys.stdin.readline
 
 n,m = map(int, input().split())
 
-res2 = [[i] for i in range(1,n+1)]
-res = []
-new = []
+indegree = [0] * (n+1)
 
-for a in range(m):
-    t,s = map(int, input().split())
-    heapq.heappush(res,[t,s])
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    x,y = map(int, input().split())
+    graph[x].append(y)
+    indegree[y] += 1
+
+print(graph,indegree)
+
+def d_sort():
+    res = []
+    q = deque()
+
+    for a in range(1,n+1):
+        if indegree[a] == 0:
+            q.append(a)
+    print(q)
+    while q:
+        t = q.popleft()
+        res.append(t)
+
+        for a in graph[t]:
+            indegree[a] -= 1
+
+            if indegree[a] == 0:
+                q.append(a)
     
+    for a in res:
+        print(a, end=' ')
 
-for _ in range(len(res2)):
-    print(heapq.heappop(res2))
+d_sort()
+
+
+
 
