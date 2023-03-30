ef5e41fe83c8ff52780ef74c1c76c9897e1610ca
minw0095 <kmu6898@naver.com>
2023-03-31 03:38:10 +0900

    100 5 ACM craft

--- python/위상정렬/1005_ACM크래프트.py
+++ python/위상정렬/1005_ACM크래프트.py
@@ -2,13 +2,12 @@ from collections import deque
 
 T = int(input())
 def top_sort():
-    res = 0
     q = deque()
 
     for a in range(1, n+1):
         if check[a] == 0:
-            q.append((a, time[a]))
-            
+            q.append(a)
+            dp[a] = time[a]
 
     while q:
         t = q.popleft()
@@ -16,9 +15,11 @@ def top_sort():
 
         for a in graph[t]:
             check[a] -= 1
+            dp[a] = max(dp[t] + time[a], dp[a])
+            print(dp)
 
             if check[a] == 0:
-                q.append((a, time[a]))
+                q.append(a)
 
 for _ in range(T):
     n,m = map(int, input().split())
@@ -27,14 +28,16 @@ for _ in range(T):
     dp = [0] * (n+1)
 
     time = [0] + list(map(int, input().split()))
-    print(time)
+
     for _ in range(m):
         x,y = map(int, input().split())
         graph[x].append(y)
         check[y] += 1
 
     end = int(input())
-    print(top_sort(),10)
+    top_sort()
+
+    print(dp[end])
 
 
             
