1a7cfe0dc297644883388ebf60bc01b14e9c51ed
minw0095 <kmu6898@naver.com>
2023-04-05 23:44:43 +0900

    RGB

--- python/dp/1149_RGB거리.py
+++ python/dp/1149_RGB거리.py
@@ -1,7 +1,10 @@
 n = int(input())
 dp = [0]*(n+1)
+dp[1] = min(map(int, input().split()))
 color = [0] * (n+1)
+print(dp)
 
-for a in range(n):
-    colors = list(map(int, input()))
-    t = min(colors)
+for a in range(n-1):
+    r,g,b = map(int, input().split())
+    t = min(r,g,b)
+    color = [[1,r], [2,g], [3,b]]
