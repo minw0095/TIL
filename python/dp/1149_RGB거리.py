ccd18d5b5f28f682bbac84ea8297a827c380d6a0
minw0095 <kmu6898@naver.com>
2023-04-06 23:46:24 +0900

    ㅇ

--- python/dp/1149_RGB거리.py
+++ python/dp/1149_RGB거리.py
@@ -1,6 +1,8 @@
 n = int(input())
 dp = [0]*(n+1)
-dp[1] = min(map(int, input().split()))
+num = [0]*(n+1)
+for a in range(len(list(map(int, input().split())))):
+
 color = [0] * (n+1)
 print(dp)
 
@@ -8,3 +10,5 @@ for a in range(n-1):
     r,g,b = map(int, input().split())
     t = min(r,g,b)
     color = [[1,r], [2,g], [3,b]]
+
+    for a in num
