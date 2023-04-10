60306d7ace0d5b79a4492abdd8096b5f3067e4ec
minw0095 <kmu6898@naver.com>
2023-04-10 22:38:37 +0900

    오등큰수

---
+++ python/dp/17299_오등큰수.py
@@ -0,0 +1,12 @@
+n = int(input())
+nums = list(map(int, input().split()))
+f = []
+stack = [-1] * n
+
+for a in nums:
+    t = nums.count(a)
+    f.append(t)
+    
+
+
+    
--- python/dp/2812_크게만들기.py
+++ python/dp/2812_크게만들기.py
@@ -3,7 +3,16 @@ t = input()
 stack = []
 
 for a in t:
-    while stack and stack[-1] < a and 
+    while stack and stack[-1] < a and k >0:
+        stack.pop()
+        k -= 1
+
+    stack.append(a)
+
+if k > 0:
+    print(''.join(stack[:-k]))
+else:
+    print(''.join(stack))
 
 
 
