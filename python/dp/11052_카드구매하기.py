7c94ce806f77019769de1f1b086369c351a8b7f2
minw0095 <kmu6898@naver.com>
2023-04-27 23:54:27 +0900

    dp

--- python/dp/11052_카드구매하기.py
+++ python/dp/11052_카드구매하기.py
@@ -1,7 +1,10 @@
 n = int(input())
-card = list(map(int, input().split()))
-dp = []
+card = [0]+list(map(int, input().split()))
+dp = [0 for _ in range(n+1)]
 price = 0
 
 for a in range(1, n+1):
-    price = max(price, a*(n//a) + )
+    for b in range(1, a+1):
+        dp[a] = max(dp[a], dp[a-b] + card[b])
+
+print(dp[n])
