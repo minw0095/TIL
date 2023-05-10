3136efe7db98f753cfbedb7f7d0474b0ece395f7
minw0095 <kmu6898@naver.com>
2023-05-10 23:58:52 +0900

    합분해

--- python/dp/2225_합분해.py
+++ python/dp/2225_합분해.py
@@ -1,5 +1,13 @@
 n, k = map(int, input().split())
 
-for a in range(n+1):
-    for b in range(k):
-        n=1
+dp = [[0]*201 for _ in range(201)]
+
+for a in range(201):
+    dp[1][a] = 1
+    dp[2][a] = a+1
+
+
+for a in range(2,201):
+    dp[a][1] = a
+    for b in range(2,201):
+        dp[a][b] = (dp[a][b-1] + dp[a-1][b])% 1000000000
