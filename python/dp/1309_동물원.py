5617cecaed24920d3fccc581ce65e93dfd3f3981
minw0095 <kmu6898@naver.com>
2023-05-12 23:36:54 +0900

    동물원

--- python/dp/1309_동물원.py
+++ python/dp/1309_동물원.py
@@ -1,2 +1,20 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
+dp = [1]*(n+1)
+
+dp[1] = 3
+
+x,y,z = 1,1,1
+
+
+for a in range(2,n+1):
+    y = x + z
+    z = x + z
+    x = dp[a-1]
+
+    dp[a] = (x+y+z) %9901
+
+print(dp[n])
