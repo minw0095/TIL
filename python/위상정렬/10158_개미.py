b3574469e9a4faa3368072464b9e2d11d550ac38
minw0095 <kmu6898@naver.com>
2023-05-19 01:09:39 +0900

    개미 시간초과

--- python/위상정렬/10158_개미.py
+++ python/위상정렬/10158_개미.py
@@ -1,4 +1,20 @@
 w,h = map(int, input().split())
 x,y = map(int, input().split())
 t = int(input())
+a,b = 1,1
 
+for _ in range(t):
+    if x == 0:
+        a = -a
+    if x == w:
+        a = -a
+    if y == 0:
+        b = -b
+    if y == h:
+        b = -b
+
+    x += a
+    y += b
+
+print(x,y)
+    
