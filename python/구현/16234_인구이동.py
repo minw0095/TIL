4bd4d4451fb486220889bee62f860060f8d7b0a1
minw0095 <kmu6898@naver.com>
2023-06-02 22:25:20 +0900

    인구이동

--- python/구현/16234_인구이동.py
+++ python/구현/16234_인구이동.py
@@ -2,11 +2,17 @@ from collections import deque
 
 n, l, r = map(int, input().split())
 
-graph = [[list(map(int, input().split()))] for _ in range(n)]
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[-1]*n for _ in range(n)]
+t= 1
 
-def bfs():
+def bfs(i,j):
     cnt = 1
+    visited[i][j] = 0
+    total = graph[i][j]
+
     q = deque()
+    q.append((i,j))
 
     while q:
         x,y = q.popleft()
@@ -17,6 +23,23 @@ def bfs():
             dx = x +nx[a]
             dy = y +ny[a]
 
-            if 0 <= nx < n and 0 <= ny < n:
-                if l <= graph[x][y] + graph[dx][dy] <= r:
-                    
+            if 0 <= dx < n and 0 <= dy < n:
+                if l <= abs(graph[x][y] - graph[dx][dy]) <= r:
+                    if visited[dx][dy] == -1:
+                        q.append((dx, dy))
+                        total += graph[dx][dy]
+                        cnt += 1
+                        visited[dx][dy] = 0
+        
+        res = total // cnt
+    
+    print(res,cnt)
+
+for a in range(n):
+    for b in  range(n):
+        if visited[a][b] == -1:
+            bfs(a,b)
+            
+
+print(t)
+        
