992582f4af4261c585e6091bbd5791c415be3f10
minw0095 <kmu6898@naver.com>
2023-06-14 23:58:48 +0900

    test;

--- python/구현/test.py
+++ python/구현/test.py
@@ -1,8 +1,23 @@
-a = []
+n = list(input().split())
+t = 0
 
-def t():
-    a.append([1,2])
+for a in len(n):
+    if n[a] == "LONG":
+        t += 16
 
-t()
-dfdf
-print(a)
+    if n[a] == "INT":
+        t += 8
+
+    if n[a] == "FLOAT":
+        if n[a+1] != "LONG" or  n[a+1] != "INT":
+        t += 4
+
+    if n[a] == "SHORT":
+        t += 2
+    
+    if n[a] == "BOOL":
+        t += 1
+
+    if t >128:
+        print("HALT")
+        break
