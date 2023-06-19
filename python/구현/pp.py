d7d4b7fc682d7e73724f184b884ee576c5a5b840
minw0095 <kmu6898@naver.com>
2023-06-19 12:34:47 +0900

    .

---
+++ python/구현/pp.py
@@ -0,0 +1,3 @@
+dic = {1:'1', 2:2,3:3}
+
+print(type(int(dic[1])))
--- python/구현/test.py
+++ python/구현/test.py
@@ -1,24 +1,7 @@
-n = list(input().split())
-t = 0
-new = []
+t= 1
 
-for a in len(n):
-    if n[a] == "LONG":
-        t += 16
+for a in range(4):
+    if a==1:
+        continue
 
-    if n[a] == "INT":
-        t += 8
-
-    if n[a] == "FLOAT":
-        if n[a+1] != "LONG" or  n[a+1] != "INT":
-        t += 4
-
-    if n[a] == "SHORT":
-        t += 2
-    
-    if n[a] == "BOOL":
-        t += 1
-
-    if t >128:
-        print("HALT")
-        break
+    print(a)
