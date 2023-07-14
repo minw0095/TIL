27e207381e321a5de9384f167a21623bf9be6a62
minw0095 <kmu6898@naver.com>
2023-07-14 23:56:33 +0900

    자바

---
+++ java/백준/1008.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        Double A = scan.nextDouble();
+        Double B = scan.nextDouble();
+        
+        System.out.println(A/B);
+        
+        scan.close();
+    }
+}
