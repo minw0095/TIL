5c4f9b179e17c2526427586c545e8fe228647435
minw0095 <kmu6898@naver.com>
2023-09-27 23:11:32 +0900

    자바

---
+++ java/백준/11382.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        long A = scan.nextLong();
+        long B = scan.nextLong();
+        long C = scan.nextLong();
+        
+        scan.close();
+        
+        System.out.print(A+B+C);
+        
+    }
+}
